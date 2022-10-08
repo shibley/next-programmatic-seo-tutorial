552f1af98c0649258ad3ff5833ac4a150898e0c0
Shibley <shibley@gmail.com>
2022-10-07 21:06:03 -0700

    no-code ui

--- nextjs-blog/components/footer.js
+++ nextjs-blog/components/footer.js
@@ -1,15 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            Copyright 2022 - No0code.tools
-        {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a> */}
+            Copyright 2022 - No-code.tools
         </footer>
     )
   }
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -18,24 +18,46 @@ export default function Home(props) {
   let i = 0;
   return (
     <>
-      <div className="container">      
-          <h1 className="hero-title">Easily Find Disc golf courses by state</h1>
-      </div>
-      <div className="linkBox-content">
-        <div className="container">
-          <div className="row">
-            {states.map(state => {
-              return (
-                <div className="col-md-4" style={{marginBottom:20}}>
-                  <a href={`/courses/${state}`} key={i++}>
-                    <div className="linkBox">
-                        <h2 className="linkBox-title">{state}</h2>
-                        See Courses
-                    </div>
-                  </a>
+      <div className="container">
+      
+        <div className="row banner">
+          <div className="col-lg-12 col-md-12 col-12">
+            <div className="row mb-4">
+              <div className="col-lg-9 col-md-9 col-9">
+                <header className="page-header">
+                  <h1 className="banner-header mb-2">
+                    <span>Discover top no-code products &amp; tools</span>
+                  </h1>
+                </header>
+                <div className="banner-sub-header">
+                  <span>No-code Tools is a curation of the best no-code products and top tips, guides &amp; how-tos surrounding them.</span>
                 </div>
-                )
-            })}
+              </div>
+
+              <div className="col-lg-3 col-3 col-md-3">
+                <img className="banner-image" src="/assets/all_things_remote-d6ee1ee9e0eff538c3abb5d6cb64bc7f1014330ec3422323447f63a4efd3721f.png" />
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+        <div className="linkBox-content">
+          <div className="container">
+            <div className="row">
+              {states.map(state => {
+                return (
+                  <div className="col-md-4" style={{marginBottom:20}}>
+                    <a href={`/courses/${state}`} key={i++}>
+                      <div className="linkBox">
+                          <h2 className="linkBox-title">{state}</h2>
+                          See Courses
+                      </div>
+                    </a>
+                  </div>
+                  )
+              })}
+            </div>
           </div>
         </div>
       </div>
